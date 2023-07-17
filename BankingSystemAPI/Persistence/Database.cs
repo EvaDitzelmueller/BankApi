4f84ba3f2310086ae5f4eacad0cdff7425b677dd
Eva Ditzelmüller <87754804+EvaDitzelmueller@users.noreply.github.com>
2023-07-17 12:57:32 +0200

    Update Database.cs

--- BankingSystemAPI/Persistence/Database.cs
+++ BankingSystemAPI/Persistence/Database.cs
@@ -5,6 +5,7 @@ namespace BankingSystemAPI.Persistence
     public static class Database
     {
         //Almost as good as a real database ;) but next time I'd propose to use Entity Framework with a relational database
+        //This obviously has severe drawbacks, like concurrency issues, code quality and testability, just a quick hack to get started without a database.
         public static List<Customer> CustomerDb { get; set; } = new List<Customer>();
         public static List<Account> AccountDb { get; set; } = new List<Account>();
     }
